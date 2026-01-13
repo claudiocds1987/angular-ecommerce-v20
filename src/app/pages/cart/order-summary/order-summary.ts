730d80953490fa813f5bdea04d57a125022ccb1a
claudiocds1987 <cla8787@gmail.com>
2026-01-13 13:45:16 -0300

    update

--- src/app/pages/cart/order-summary/order-summary.ts
+++ src/app/pages/cart/order-summary/order-summary.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../../shared/services/cart-service';
 import { PrimaryButton } from '../../../shared/components/primary-button/primary-button';
 import { CartDto, MercadoPagoService } from '../../../shared/services/mercado-pago';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-var
-declare var MercadoPago: any;
+//declare var MercadoPago: any;
 
 @Component({
     selector: 'app-order-summary',
@@ -19,6 +20,42 @@ export class OrderSummary {
     private _mercadoPagoService = inject(MercadoPagoService);
 
     onCheckout() {
+    // 1. Mapear datos
+    const dataParaBackend: CartDto = {
+        items: this.cartService.cart().map(product => ({
+            name: product.title,
+            quantity: product.quantity,
+            price: product.price
+        }))
+    };
+
+    this._mercadoPagoService.createPreference(dataParaBackend).subscribe({
+        next: (res) => {
+            console.log('Preference ID recibido:', res.id);
+            
+            const windowMP = (window as any).MercadoPago;
+
+            if (windowMP) {
+                const mp = new windowMP('TEST-c3a31b76-ad93-4877-b3a9-fdefa8357b42');
+                mp.checkout({
+                    preference: {
+                        id: res.id
+                    },
+                    autoOpen: true
+                });
+            } else {
+                console.error('El SDK de Mercado Pago no está cargado en window');
+                alert('Error: No se pudo cargar la pasarela de pago. Reintente en unos segundos.');
+            }
+        },
+        error: (err) => {
+            console.error('Error al crear la preferencia', err);
+            alert('Hubo un error al conectar con el servidor');
+        }
+    });
+}
+
+    /* onCheckout() {
         // 1. Mapear los datos del carrito
         const dataParaBackend: CartDto = {
             items: this.cartService.cart().map((product) => ({
@@ -53,5 +90,5 @@ export class OrderSummary {
                 alert('Hubo un error al procesar el pago');
             },
         });
-    }
+    } */
 }
