490e476d59fd71f81ecf076347e058e0b1047a34
claudiocds1987 <cla8787@gmail.com>
2026-05-14 14:22:26 -0300

    update styles

--- src/app/pages/admin/product-form-admin/product-form-admin/product-form-admin.ts
+++ src/app/pages/admin/product-form-admin/product-form-admin/product-form-admin.ts
@@ -36,6 +36,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -69,6 +70,7 @@ interface ImageFormRow {
     MatCardModule,
     MatChipsModule,
     MatDividerModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
