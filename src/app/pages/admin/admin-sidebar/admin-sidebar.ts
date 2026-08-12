04a14ea54c80d71d06a5356a5bf8dca08fb5a1b6
claudiocds1987 <cla8787@gmail.com>
2026-08-12 15:44:15 -0300

    implement isActive to mark active route

--- src/app/pages/admin/admin-sidebar/admin-sidebar.ts
+++ src/app/pages/admin/admin-sidebar/admin-sidebar.ts
@@ -11,12 +11,32 @@ import { AdminDashboardService } from '../admin-dashboard/admin-dashboard-servic
   styleUrl: './admin-sidebar.scss',
 })
 export class AdminSidebar {
-  sidebarCollapsed = signal(false);
+  isSidebarCollapsed = signal(false);
+  activeRoute = signal<string>('');
   readonly dashboardService = inject(AdminDashboardService);
   private readonly _router = inject(Router);
 
+  // La función isActive detecta si estás en la ruta que se le indicó,
+  // y si es así aplica la clase azul al botón para que el usuario sepa
+  // en qué sección del sistema se encuentra.
+  // Se llama en el HTML, por ejemplo:
+  // [class.bg-blue-600]="isActive('/admin/product-extra-attribute-definition')"
+
+  isActive(path: string): boolean {
+    return this._router.isActive(path, {
+      paths: 'exact',
+      queryParams: 'ignored',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    });
+  }
+
   toggleSidebar(): void {
-    this.sidebarCollapsed.update((v) => !v);
+    this.isSidebarCollapsed.update((v) => !v);
+  }
+
+  goToDashboard(): void {
+    this._router.navigate(['admin']);
   }
 
   goToProducts(): void {
